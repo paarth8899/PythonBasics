0f945d53f0d32efb43970d367f13fffbc488060e
paarth8899 <paarth8573.beaift25@chitkara.edu.in>
2025-08-21 19:07:48 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,27 @@
+# int form
+a = 23
+b= 79
+print(a+b)
+
+# float form 
+a =79.9
+b= 84.98
+print(a+b)
+
+
+# long form 
+a = 239400
+b= 794785890
+print(a+b)
+
+# char form 
+a = "r"
+b= "u"
+print(a+b)
+
+name = input("Enter your name")
+
+age = int(input("Enter your age"))
+
+print("Your age is ", age)
+ 
---
+++ day2.py
@@ -0,0 +1,21 @@
+a= int(input("enter the first number"))
+b=int(input("enter the second number"))
+print("the difference between two number is"(a-b))
+
+
+
+
+
+
+a=int(input("enter your marks"))
+if 90<=a:
+  print("a+")
+elif 90>a>=75:
+  print("a")
+elif 75>a>60:
+  print("b")
+elif 40<a<50:
+  print("c")
+
+else:
+  print("failed")
