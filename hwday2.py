0ba263476525f113683d784e068ef1cce343da3e
paarth8899 <paarth8573.beaift25@chitkara.edu.in>
2025-08-26 10:52:27 +0530

    Add files via upload

---
+++ hwday2.py
@@ -0,0 +1,15 @@
+print("1. add")
+print("2. subtract ")
+print("3. multiply")
+print("4. divide")
+
+print("enter the desired operation you want to perform")
+
+
+choice = input("enterchoice 1/2/3/4/").strip
+
+b=int(input("enter the first number"))
+
+c=int(input("enter the second number"))
+
+print (choice)
